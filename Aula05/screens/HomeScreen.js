1680887a22b6bf2f9ea451833fc3219805c67c36
SARAH SILVA <126732728+sarahsilva01@users.noreply.github.com>
2025-09-11 23:35:36 +0000

    (11/09) Atividade Aula 05

---
+++ Aula05/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import { View, Image, StyleSheet, Button } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.imagem} />
+      <Image
+        source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        style={{ width: 100, height: 100 }}
+      />
+      <FontAwesome name="home" size={80} color="blue" />
+      <MaterialCommunityIcons name="horse-variant-fast" size={80} color="green" />
+      <Button
+        title="Perfil"
+        onPress={() => navigation.navigate('Perfil')}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  imagem: { width: 200, height: 200, resizeMode: 'contain' }
+});
